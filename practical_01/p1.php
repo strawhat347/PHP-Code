39d34b3cce287c2b7b090dab17442a9c4a151ce7
strawhat347 <nealbaroliya1412007@gmail.com>
2026-02-15 23:24:01 +0530

    Update p1.php

--- practical_01/p1.php
+++ practical_01/p1.php
@@ -1,30 +1,40 @@
-<?php
+<?php 
 
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------<br>";
-echo "Write a PHP script to demonstrate encapsulation using private data members and public getter and setter methods. <br>";
+echo "Write a PHP script to display a simple message using echo/print and demonstrate variables and data types using the var_dump() function. <br>";
 echo "Name : Neal Baroliya <br>";
 echo "Enrollment Number : 24082291002 <br>";
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------<br>";
 
+    echo "Hello, world! <br>";
+    print("Welcome to php <br><br>");  
 
-    class Student {
-        private $name;
-        private $rollNo;
+    $Name = "XYZ";
+    $Age = 21;
+    $Height = 5.6;
+    $isAlive = true;
+    $Grade = 'C';
 
-        public function setData($n, $r) {
-   
-            $this->name = $n;
-            $this->rollNo = $r;
-        }
+    $arr = [1,2,3,4,5];
 
-        public function getData() {
-        
-            echo "Name: " . $this->name . "<br>";
-            echo "Roll No: " . $this->rollNo;
-        }
-    }
+    var_dump($Name);
+    echo "<br><br>";
     
-    $obj = new Student();
-    $obj->setData("Rahul", 101);
-    $obj->getData();
+    var_dump($Age);
+    echo "<br><br>";
+    
+    var_dump($Height);
+    echo "<br><br>";
+    
+    var_dump($isAlive);
+    echo "<br><br>";
+    
+    var_dump($Grade);
+    echo "<br><br>";
+
+    var_dump($arr);
+    echo "<br><br>";
+    
+    
+    
 ?>
